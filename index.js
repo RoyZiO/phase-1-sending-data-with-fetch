f0904c160c21da50f0a9517e50c98794c53b4839
RoyZiO <nziokinroy@gmail.com>
2022-12-07 12:11:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Add your code here
+function submitData() {
+
+    const data = {
+        name: 'Steve',
+        email: 'steve@steve.com',
+    }
+
+    const userObject = {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+
+        body: JSON.stringify(data),
+    };
+
+    let bigFetch = fetch('http://localhost:3000/users', userObject)
+
+    .then (function(response) {
+        return response.json();
+    })
+    .then( function(object) {
+        document.body.innerHTML = object['id'];
+    }) 
+
+    .catch( function ( error ) {
+
+        document.body.innerHTML = error.message
+
+    } );
+
+    return bigFetch;
+
+   
+}
+
